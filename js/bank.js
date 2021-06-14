e5b54ba31110671cd0c2db06b1655a8afd10be7f
dannida1 <daniel.agbey@amalitech.org>
2021-06-14 08:02:39 +0000

    initial commit

---
+++ js/bank.js
@@ -0,0 +1,8 @@
+let username = document.getElementById('username').value;
+let password = document.getElementById('password').value;
+
+document.getElementById('login_btn').addEventListener('click', validate);
+
+function validate() {
+    alert('Please enter');
+}
